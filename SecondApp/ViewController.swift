0f5cadbe9c2b36dc165863fe579e15dca3c022d7
陈岩 <chenyan@chenyandeMacBook-Pro.local>
2018-01-03 11:25:15 +0800

    save

--- SecondApp/ViewController.swift
+++ SecondApp/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        title = "ss"
+        title = "ssdd"
         // Do any additional setup after loading the view, typically from a nib.
     }
 
